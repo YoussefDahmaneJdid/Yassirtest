0672c2bde18b59851488f499f62677dce98c10b6
Youssef-Dahmane Jdid <youssef@shopsandco.com>
2022-10-18 11:24:18 +0100

    testintegration push

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect(process.env.MONGODB_URI);
 
-//mongodb+srv://admin:azAZaz123@cluster0.5zenl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
 module.exports = {
   mongoose
 }
--- test/serverIntergration.test.js
+++ test/serverIntergration.test.js
@@ -1,11 +1,11 @@
 
 
 const request = require('supertest');
-const {ObjectID} = require('mongodb');
+
 var {app} = require('../app');
 
 var expect = require('Chai').expect
-const { Record } = require('../schema/record');
+
 
 //////////////////////////////////////////////////:
 
@@ -17,7 +17,7 @@ describe('/getRecord',()=>{
      .get('/air/48.856613/2.352222')
      .expect(200)
      .expect((res,req)=>{
-        console.log(req);
+        console.log(req); 
         console.log(res.text); 
         res = JSON.parse(res.text);
         console.log(res.data.city);
